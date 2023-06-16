dbb0a4583908250bf38f23b8e9a8e3d1b53d5a12
Martin Lopes <martinrodrigueslopess@gmail.com>
2023-06-16 14:40:49 -0400

    Versão 3: Programação Orientada a Objetos

--- Prova/Form1.cs
+++ Prova/Form1.cs
@@ -26,48 +26,14 @@ namespace Prova
             //string t_salario = tx_salario.Text;
 
             double salario = Convert.ToDouble(tx_salario.Text);
-            double periculosidade;
-            double ti;
-            double valealimentacao;
-            double valetransporte;
-            double irpf = 0.0;
-
+            
             //MessageBox.Show($"{salario}");
 
-            if (funcao == "Médico")
-            {
-                periculosidade = salario * 0.20;
-                salario = salario + periculosidade;
-                //MessageBox.Show($"salario inicial: {salario}");
-            }
+            Funcionario f = new Funcionario(matricula, cpf, nome, funcao, salario);
 
-            else if (funcao == "TI")
-            {
-                ti = 5600;
-                salario = salario + ti;
-                //MessageBox.Show($"salario inicial: {salario}");
-            }
-
-            else if(funcao == "ADM")
-            {
-                valetransporte = salario * 0.15;
-                salario = salario + valetransporte;
-
-                valealimentacao = 200;
-
-                salario = salario + valealimentacao;
-                //MessageBox.Show($"salario inicial: {salario}");
-            }
-
-            if(salario > 5000)
-            {
-                irpf = salario * 0.17;
-                salario = salario - irpf;
-                //MessageBox.Show($"salario final: {salario}");
-            }
-
-            lb_salariototal.Text = $"Salário total: {salario}";
-            lb_irpf.Text = $"IRPF: {irpf}";
+            (double n1, double n2) = f.Salario();
+            lb_salariototal.Text = $"Salário total: {n1}";
+            lb_irpf.Text = $"IRPF: {n2}";
         }
     }
 }
---
+++ Prova/Funcionario.cs
@@ -0,0 +1,66 @@
+﻿using System;
+public class Funcionario
+{
+    string matricula;
+    string cpf;
+    string nome;
+    string funcao;
+    double salario;
+
+    double periculosidade;
+    double ti;
+    double valealimentacao;
+    double valetransporte;
+    double irpf = 0.0;
+
+    public Funcionario(string matricula, string cpf, string nome, string funcao, double salario)
+    {
+        this.matricula = matricula;
+        this.cpf = cpf;
+        this.nome = nome;
+        this.funcao = funcao;
+        this.salario = salario;
+    }
+
+    public (double, double) Salario()
+    {
+        if (funcao == "Médico")
+        {
+            periculosidade = salario * 0.20;
+            salario = salario + periculosidade;
+            //return (salario, irpf);
+            //MessageBox.Show($"salario inicial: {salario}");
+        }
+
+        else if (funcao == "TI")
+        {
+            ti = 5600;
+            salario = salario + ti;
+            //return (salario, irpf);
+            //MessageBox.Show($"salario inicial: {salario}");
+        }
+
+        else if (funcao == "ADM")
+        {
+            valetransporte = salario * 0.15;
+            salario = salario + valetransporte;
+
+            valealimentacao = 200;
+
+            salario = salario + valealimentacao;
+            //return (salario, irpf);
+            //MessageBox.Show($"salario inicial: {salario}");
+        }
+
+        if (salario > 5000)
+        {
+            irpf = salario * 0.17;
+            salario = salario - irpf;
+            //return (salario, irpf);
+            //MessageBox.Show($"salario final: {salario}");
+        }
+
+        return (salario, irpf);
+    }
+
+}
